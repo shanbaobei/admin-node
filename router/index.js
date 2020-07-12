4494f86c18ecfd7f2aef3ce8283d982aac2a4050
merciqiao <812858143@qq.com>
2020-07-12 22:32:57 +0800

    1 、初始化项目   1.1  安装boom依赖    1.2 对接口报错进行处理

---
+++ router/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const boom = require('boom')
+const userRouter = require('./user')
+const {
+    CODE_ERROR
+} = require('../utils/constant')
+
+//注册路由
+const router = express.Router()
+router.get('/', function (req, res) {
+    res.send('欢迎学习小慕读书')
+})
+router.use('/user',userRouter)//对user下的所有请求进行处理
+// 集中处理404请求
+router.use((req,res,next) => {
+    next(boom.notFound('接口不存在'))
+})
+//错误向下传递
+//自定义路由异常处理中间件
+// router.use((err, req, res, next) => {
+//       const msg = (err && err.message) || '系统错误'
+//       const statusCode = (err.output && err.output.statusCode) || 500;
+//       const errorMsg = (err.output && err.output.payload && err.output.payload.error) || err.message
+//       res.status(statusCode).json({
+//         code: CODE_ERROR,
+//         msg,
+//         error: statusCode,
+//         errorMsg
+//       })
+//     }
+//   )
+router.use((err,req,res,next) => {
+    const msg = (err && err.message) || '系统处理错误'
+    const statusCode = (err.output && err.output.statusCode) || 500;
+    const errorMsg = (err.output && err.output.payload && err.output
+        .payload.error) || err.message
+        res.status(statusCode).json({
+            code:CODE_ERROR,
+            msg,
+            error:statusCode,
+            errorMsg
+        })
+       
+})
+
+
+
+
+
+// 导出router 作用是处理路由的监听
+module.exports = router
---
+++ utils/constant.js
@@ -0,0 +1,3 @@
+module.express = {
+    CODE_ERROR:-1
+}
