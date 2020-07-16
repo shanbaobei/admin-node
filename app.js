a26f5880ea50b9ee44498002b2f5f4c46f4497fa
MININT-KDD4KI4\Administrator <1249556847@qq.com>
2020-07-16 15:58:58 +0800

    1 更改node端口号

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')   //把express引用进来
 const router = require('./router')
 const app = express()    //创建一个app，这个app就是一个express应用
+const fs = require('fs')
+const http = require('http')
+
 
 app.use('/',router)
 // app.get('/',function(req,res){
 //     res.send('hellow world')
 // })
 
-const server = app.listen(5000,function(){    //将这个接口监听起来，并返回这个服务
+const server = app.listen(18082,function(){    //将这个接口监听起来，并返回这个服务
     const { address,port} = server.address()
     console.log('http服务启动成功:http://%s:%s',address,port)
 })
