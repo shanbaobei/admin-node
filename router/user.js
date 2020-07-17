371a09b1c8440953d49410030e98a075490e40df
MININT-KDD4KI4\Administrator <1249556847@qq.com>
2020-07-17 17:36:01 +0800

    1 对登入失败做判断

--- router/user.js
+++ router/user.js
@@ -4,7 +4,13 @@ const Result = require('../models/Result')
 
 router.post('/login', function(req, res) {
     console.log(req.body)
-    new Result().success(res)
+    const {username,password} = req.body
+    if ( username === 'admin' && password === '11111'){
+        new Result( '登录成功').success(res)
+    }else{
+        new Result( '登录失败 ').fail(res)
+    }
+    
 })
 router.get('/info',function(req,res,next){
     res.json('user info...')
